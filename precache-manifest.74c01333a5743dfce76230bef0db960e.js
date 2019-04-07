73267dbd56967f7fb5fbb39b5387cc0a222fe777
Horia <gug.horia@gmail.com>
2019-04-07 22:12:06 +0300

    Updates

---
+++ precache-manifest.74c01333a5743dfce76230bef0db960e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ed0bb0bb524bf8368de",
+    "url": "/fortheserve/static/js/runtime~main.b74ec5eb.js"
+  },
+  {
+    "revision": "46f43a62d55c167f5abd",
+    "url": "/fortheserve/static/js/main.dc0ad9b6.chunk.js"
+  },
+  {
+    "revision": "15a9b2c105c6e72e44b3",
+    "url": "/fortheserve/static/js/2.99a4df26.chunk.js"
+  },
+  {
+    "revision": "46f43a62d55c167f5abd",
+    "url": "/fortheserve/static/css/main.00d40ca0.chunk.css"
+  },
+  {
+    "revision": "15a9b2c105c6e72e44b3",
+    "url": "/fortheserve/static/css/2.959e179c.chunk.css"
+  },
+  {
+    "revision": "8e409c71563756de6c7e1ecb228007c5",
+    "url": "/fortheserve/index.html"
+  }
+];
