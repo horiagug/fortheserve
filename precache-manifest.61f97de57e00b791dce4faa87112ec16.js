22697772a43f6609b32cc6518e1147723c0924df
Horia <gug.horia@gmail.com>
2019-05-04 10:56:23 +0300

    Updates

--- precache-manifest.61f97de57e00b791dce4faa87112ec16.js
+++ precache-manifest.61f97de57e00b791dce4faa87112ec16.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/fortheserve/static/js/runtime~main.b74ec5eb.js"
   },
   {
-    "revision": "bc842cc547d4ead4761d",
-    "url": "/fortheserve/static/js/main.bbd07439.chunk.js"
+    "revision": "47fc218a045b0510ac59",
+    "url": "/fortheserve/static/js/main.78c0a1d1.chunk.js"
   },
   {
-    "revision": "23a6ab4358c4d6202ba8",
-    "url": "/fortheserve/static/js/2.b85d916e.chunk.js"
+    "revision": "dee27f0b65639a42e246",
+    "url": "/fortheserve/static/js/2.b55e7885.chunk.js"
   },
   {
-    "revision": "bc842cc547d4ead4761d",
+    "revision": "47fc218a045b0510ac59",
     "url": "/fortheserve/static/css/main.c5404bff.chunk.css"
   },
   {
-    "revision": "23a6ab4358c4d6202ba8",
+    "revision": "dee27f0b65639a42e246",
     "url": "/fortheserve/static/css/2.2d284ed1.chunk.css"
   },
   {
-    "revision": "8b8c3e43c386b8604bc4bdf123038aaf",
+    "revision": "8f295c33649f5a439d97daf3317c4480",
     "url": "/fortheserve/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fortheserve/precache-manifest.68843ab97fc0aee1412092968717ec25.js"
+  "/fortheserve/precache-manifest.61f97de57e00b791dce4faa87112ec16.js"
 );
 
 workbox.clientsClaim();
